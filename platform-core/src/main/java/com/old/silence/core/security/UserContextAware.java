fa26cc7e42d3a754ba6113b7e36d65cf36b55558
张孟伟 <1436373010@qq.com>
2025-09-17 11:41:16 +0800

    init project

--- platform-core/src/main/java/com/old/silence/core/security/UserContextAware.java
+++ platform-core/src/main/java/com/old/silence/core/security/UserContextAware.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 /**
  * @author moryzang
  */
-public interface UserContextAware {
+public interface UserContextAware<T> {
 
     /**
      * 获取当前用户名
      * @return 当前用户名，如果没有认证用户则返回默认值
      */
-    Optional<String> getCurrentAuditor();
+    Optional<T> getCurrentAuditor();
 }
---
+++ platform-webmvc/src/main/java/com/old/silence/webmvc/data/UserHeaderAuditorAware.java
@@ -0,0 +1,38 @@
+package com.old.silence.webmvc.data;
+
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import com.old.silence.core.security.UserContextAware;
+
+/**
+ * @author moryzang
+ */
+public class UserHeaderAuditorAware implements UserContextAware<String> {
+
+    private final Optional<String> defaultAuditorAwareName;
+
+    public UserHeaderAuditorAware(String defaultAuditorAwareName) {
+        this.defaultAuditorAwareName = Optional.of(defaultAuditorAwareName);
+    }
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (!(requestAttributes instanceof ServletRequestAttributes)) {
+            return this.getDefaultAuditorAwareName();
+        } else {
+            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+            String headerUsername = request.getHeader("X-Silence-User-Header");
+            return StringUtils.isBlank(headerUsername) ? this.getDefaultAuditorAwareName() : Optional.of(headerUsername);
+        }
+    }
+
+    private Optional<String> getDefaultAuditorAwareName() {
+        return this.defaultAuditorAwareName;
+    }
+}
